bb24f1f0447f31dc26bb1b850ff67da5d19b110b
Luka Milenković <183858411+lukamilenkovicc@users.noreply.github.com>
2026-01-31 13:55:01 +0100

    feat(database): Complete database setup with Alembic and seeded data

---
+++ backend/app/models/__init__.py
@@ -0,0 +1,18 @@
+
+
+from app.models.userRole import UserRole
+from app.models.user import User
+from app.models.mode import Mode
+from app.models.chat import Chat
+from app.models.message import Message
+from app.models.document import Document
+
+
+__all__ = [
+    "UserRole",
+    "User", 
+    "Mode",
+    "Chat",
+    "Message",
+    "Document"
+]
